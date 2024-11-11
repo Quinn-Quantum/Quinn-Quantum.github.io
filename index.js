25f61bdd814e19ffef16cb1df06594b11e6c464a
Quinn-Quantum <79361383+Quinn-Quantum@users.noreply.github.com>
2024-11-11 22:04:57 +0100

    add: responsive webdesign for menu, pnp

---
+++ index.js
@@ -0,0 +1,15 @@
+
+function showMenu(){
+    navMenu = document.getElementById('navMenu')
+    navClose = document.getElementById('navClose')
+    navMenu.style.display = 'block'
+    navClose.style.display = 'block'
+}
+
+function removeMenu(){
+    navMenu = document.getElementById('navMenu')
+    navClose = document.getElementById('navClose')
+    navMenu.style.display = 'none'
+    navClose.style.display = 'none'
+}
+
