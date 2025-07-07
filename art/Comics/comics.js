8ee2f647f53db8708c3eb1410c9b4d06dcded5f4
Quinn-Quantum <79361383+Quinn-Quantum@users.noreply.github.com>
2025-07-07 19:25:18 +0200

    Test:comic

--- art/Comics/comics.js
+++ art/Comics/comics.js
@@ -1,16 +1,16 @@
 const images = [
-    "./assets/warten/Seite_01.png",
-    "./assets/warten/Seite_2.png",
-    "./assets/warten/Seite_3.png"
+    "./images/warten/Seite_01.png",
+    "./images/warten/Seite_2.png",
+    "./images/warten/Seite_3.png"
   ];
   
   let currentPage = 0;
 
-// function updateImage() {
-//   const img = document.getElementById("comic-image");
-//   img.src = images[currentPage];
-//   document.getElementById("page-indicator").textContent = `${currentPage + 1} / ${images.length}`;
-// }
+function updateImage() {
+  const img = document.getElementById("comic-image");
+  img.src = images[currentPage];
+  document.getElementById("page-indicator").textContent = `${currentPage + 1} / ${images.length}`;
+}
 
 function nextPage() {
   if (currentPage < images.length - 1) {
